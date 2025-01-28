9a47047f6cd8c3713ddce0a1a9149b6c22c8edeb
andrey-gazzaev <andrey.gazzaev@gmail.com>
2025-01-28 02:39:29 +0000

    https://github.com/andrey-gazzaev/SuperCrossPlatformLib/commit/9c99aa42431163bf51c50678693fa3dbadb13a76

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+export declare namespace calculationUtils {
+  export function ConvertMinutesToHours(minutes: string): string;
+  export function ConvertHoursToMinutes(hours: string): string;
+  export function SafeDivide(
+    firstValue: string,
+    secondValue: string,
+    fallbackValue?: string
+  ): string;
+}
