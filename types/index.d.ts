20b197b3bd04922d57c0ee3a74ac5d2a224476a9
andrey-gazzaev <andrey.gazzaev@gmail.com>
2025-01-28 05:23:25 +0000

    https://github.com/andrey-gazzaev/SuperCrossPlatformLib/commit/0971ace29e3416bc2b8c5c8731e052faeaa1f0a9

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-declare module "calculation-utils" {
+declare module "super-cross-platform-lib" {
   export namespace CalculationUtils {
     export function ConvertMinutesToHours(minutes: string): string;
     export function ConvertHoursToMinutes(hours: string): string;
