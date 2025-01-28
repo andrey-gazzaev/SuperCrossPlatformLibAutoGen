3a47986127fb50a21f750bff6555bbf2528497d9
andrey-gazzaev <andrey.gazzaev@gmail.com>
2025-01-28 02:22:33 +0000

    https://github.com/andrey-gazzaev/SuperCrossPlatformLib/commit/81041cd59b9ff45f4475167fc96ab08d70c21aeb

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const { getAssemblyExports, getConfig, runMain } = await dotnet
 const config = getConfig();
 const exports = await getAssemblyExports(config.mainAssemblyName);
 
-// TODO: Change this to just export the functions.
-window.calculationUtils = exports;
+export const calculationUtils = exports;
 
 // run the C# Main() method and keep the runtime process running and executing further API calls
 await runMain();
